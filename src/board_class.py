66491c2cb84c5bd5340687ce788448ebf230229f
Yannick <yannickbensimon@gmail.com>
2025-03-25 10:30:26 -0400

    Added the rectangles

--- src/board_class.py
+++ src/board_class.py
@@ -8,7 +8,7 @@ class Board:
         self.width = width
         self.height = height
         self.dim = dim
-        self.diff = (coord_start - coord_start)//dim
+        self.diff = (coord_end - coord_start)//dim
 
         self.board = [[0] * (dim + 3)] * dim
         self.displated = [False, False, False]
@@ -31,3 +31,22 @@ class Board:
 
 
 b=Board(50, 550, 600, 800, 10)
+
+
+'''
+c.create_line(BOARD_START, BOARD_END, BOARD_END, BOARD_END, fill="black", width=5)
+    c.create_line(BOARD_END, BOARD_START, BOARD_END, BOARD_END, fill="black", width=5)
+    c.create_line(BOARD_END, BOARD_START, BOARD_START,BOARD_START, fill="black", width=5)
+    c.create_line(BOARD_START, BOARD_END, BOARD_START,BOARD_START, fill="black", width=5)
+
+    c.create_rectangle(BOARD_START, BOARD_END +10, BOARD_START + 150, BOARD_END + 150, fill="white") 
+    c.create_rectangle(BOARD_START + 150, BOARD_END +10, BOARD_START + 300, BOARD_END + 150, fill="white") 
+    c.create_rectangle(BOARD_START + 300, BOARD_END +10, BOARD_START + 450, BOARD_END + 150, fill="white") 
+        
+for x in range(BOARD_START, BOARD_END, DIFF):
+        c.create_line(x, BOARD_START, x, BOARD_END, fill="black", width=5)
+        c.create_line(BOARD_START, x, BOARD_END, x, fill="black", width=5)
+
+
+'''
+
