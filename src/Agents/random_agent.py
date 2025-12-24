80b71d4f349de4bd6d77e899c15eea667d01abbe
Yannick <yannickbensimon@gmail.com>
2025-12-24 16:42:45 -0500

    Boiler platae for runnign agents. agent store and argparse

---
+++ src/Agents/random_agent.py
@@ -0,0 +1,16 @@
+'''Trial run. This agent will pick a random move'''
+
+import random
+
+from agent import Agent
+from store import register_agent
+
+@register_agent("random_agent")
+class RamdomAgent(Agent):
+    def __init__(self, game_interface):
+        super.__init__(game_interface)
+
+    def choose_move(self):
+       possible_moves = super.get_possible_moves() 
+       return random.choice(possible_moves)
+        
---
+++ src/store.py
@@ -0,0 +1,19 @@
+'''
+Code from 
+https://github.com/davaus80/COMP424-Fall2025/blob/main/store.py
+'''
+
+AGENT_REGISTRY = {}
+
+# Define decorator for registering game agents
+def register_agent(agent_name=""):
+    def decorator(func):
+        if agent_name not in AGENT_REGISTRY:
+            AGENT_REGISTRY[agent_name] = func
+        else:
+            raise AssertionError(
+                f"Agent {AGENT_REGISTRY[agent_name]} is already registered."
+            )
+        return func
+
+    return decorator
