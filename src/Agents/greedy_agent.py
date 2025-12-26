e19a4c08b78bc3439087d868ecf46f6e5c186ccb
Yannick <yannickbensimon@gmail.com>
2025-12-26 16:34:47 -0500

    Added functionality for greedily choosing columns

--- src/Agents/greedy_agent.py
+++ src/Agents/greedy_agent.py
@@ -30,6 +30,18 @@ class GreedyAgent(Agent):
                             best_move = move
                         cur_dist_to_clear = 0 
 
+            for col in zip(*dummy_board): #zip(*... to get the columns
+                cur_dist_to_clear = 0
+                for cell in col:
+                    if not cell:
+                        cur_dist_to_clear += 1
+                    else: #If a cell is filled, check if this contiguous block is smaller then the min found thus far set the cur distance to 0
+                        if cur_dist_to_clear < min_dist_to_clear:
+                            min_dist_to_clear = cur_dist_to_clear
+                            best_move = move
+                        cur_dist_to_clear = 0 
+
+
         assert best_move, "No best move found"        
         return best_move
             
