f0fe67deb02f2d3ed81541f083dac3b53ebfd6f1
Yannick <yannickbensimon@gmail.com>
2025-12-29 09:12:23 -0500

    Removed os and sys dependencies for import

--- src/Agents/agent.py
+++ src/Agents/agent.py
@@ -1,11 +1,3 @@
-import sys
-import os
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-parent_dir = os.path.dirname(current_dir)
-
-sys.path.append(parent_dir)
 
 from Move import Move
 
